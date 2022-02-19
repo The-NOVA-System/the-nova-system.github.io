cc830b4931a9cc947e9ab5a8f787bfc5d3ce3716
Garv Shah <gshah.6110@gmail.com>
2022-02-19 19:25:18 +1100

    fixed some build errors

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,18 @@
 // vite.config.js
 const { resolve } = require('path')
 const { defineConfig } = require('vite')
+import path from "path";
+
+console.log(__dirname);
 
 module.exports = defineConfig({
     root: 'src',
     build: {
+        outDir: path.join(__dirname, "dist"),
         rollupOptions: {
             input: {
-                main: resolve(__dirname, 'index.html'),
-                nested: resolve(__dirname, 'privacy-policy/index.html')
+                main: resolve(__dirname, "src", 'index.html'),
+                nested: resolve(__dirname, "src", 'privacy-policy/index.html')
             }
         }
     }
