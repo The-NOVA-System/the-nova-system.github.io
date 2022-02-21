fe39ade7568a422614657f92bd20c8d69ff83cbd
Garv Shah <gshah.6110@gmail.com>
2022-02-21 18:02:33 +1100

    added in blog page

--- src/main.js
+++ src/main.js
@@ -7,14 +7,81 @@ import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
 let contact = 0;
 let team = 0;
 let home = 0;
+let blog = 0;
+
+// this script loads atom/rss feed to the "block-content" div
+
+function makeBlogPost() {
+  var postElem = document.createElement("div")
+  var CSSattr = document.createAttribute("class")
+  CSSattr.value = "clean-blog-post"
+  postElem.setAttributeNode(CSSattr)
+  return postElem;
+}
+
+//fetches the posts from the rss feed
+function fetchPosts() {
+  const request = new XMLHttpRequest();
+  const getUrl = window.location;
+  const baseUrl = getUrl.protocol + "//" + getUrl.host + "/" + getUrl.pathname.split('/')[1];
+  const ATOM_FEED_URL = "https://the-nova-system.github.io/blog/feed.xml";
+  const ERROR_MSG = "Uh Oh: Cannot load posts at the moment :("
+  request.open("GET", ATOM_FEED_URL, true);
+
+  request.send();
+  request.onreadystatechange = function() {
+    if (this.readyState === 4 && this.status === 200) {
+      loadPosts(this.responseXML);
+    } else if (this.readyState === 4) {
+      let textbox = makeBlogPost();
+      textbox.innerHTML = ERROR_MSG;
+      document.querySelector("#page > main > section > div > div.block-content").appendChild(textbox)
+    }
+  };
+}
+
+
+function loadPosts(feedXml) {
+  const listPosts = feedXml.getElementsByTagName("feed")[0].getElementsByTagName("entry");
+
+  for (let i=0; i<listPosts.length; i++) {
+    let post = listPosts.item(i); //gets the post from the list
+
+    //extracts the information needed from the post
+    const link = post.getElementsByTagName("link")[0].attributes.getNamedItem("href").value;
+    const excerpt = post.getElementsByTagName("summary")[0].textContent;
+    const title = post.getElementsByTagName("title")[0].textContent;
+    let published = post.getElementsByTagName("published")[0].textContent;
+    published = published.substring(0, 10).replaceAll("-","/") //cleans to YYYY-MM-DD
+    const author = post.getElementsByTagName("author")[0].textContent;
+
+    let postElem = makeBlogPost(); //creates an empty textbox from the function above
+
+    //this code just creates the html for our textbox using the variables defined above, and then pushes it the inner HTML of the post element
+    postElem.innerHTML = `
+    <h3>${title}</h3>
+    <div class=\"info\">
+        <span style="color: #ffffff">By ${author} - ${published}</span>
+    </div>
+    <p>${excerpt}</p>
+    <a class="btn btn-outline-primary btn-sm" role="button" href="${link}" style="color: #ffffff; border-color: #cad4e0;">
+        Read More
+    </a>
+    <div style="padding-bottom: 60px"></div>
+    `
+
+    //finally, here we push the final post element into the block-content div
+    document.querySelector("#blog-page > section > div > section > div > div.row.people.about").appendChild(postElem)
+  }
+}
 
 const scene = new THREE.Scene();
 
 const camera = new THREE.PerspectiveCamera(
-  75,
-  window.innerWidth / window.innerHeight,
-  0.1,
-  1000
+    75,
+    window.innerWidth / window.innerHeight,
+    0.1,
+    1000
 );
 
 const renderer = new THREE.WebGLRenderer({
@@ -95,6 +162,17 @@ const torus = new THREE.Mesh(geometry, material);
 
 scene.add(torus);
 
+function addGeometry() {
+  const geometry = new THREE.TorusGeometry(10, 3, 16, 100);
+  const material = new THREE.MeshBasicMaterial({ color: 0xff6347, wireframe: true });
+  const torus = new THREE.Mesh(geometry, material);
+  torus.position.set(-10, -30, 18)
+
+  scene.add(torus);
+}
+
+addGeometry();
+
 // Lights
 
 const pointLight = new THREE.PointLight(0xffffff);
@@ -117,8 +195,8 @@ function addStar() {
   const star = new THREE.Mesh(geometry, material);
 
   const [x, y, z] = Array(3)
-    .fill()
-    .map(() => THREE.MathUtils.randFloatSpread(100));
+      .fill()
+      .map(() => THREE.MathUtils.randFloatSpread(100));
 
   star.position.set(x, y, z);
   scene.add(star);
@@ -136,8 +214,8 @@ scene.background = spaceTexture;
 const logoTexture = new THREE.TextureLoader().load("https://the-nova-system.github.io/extra-assets/logo.png");
 
 const logo = new THREE.Mesh(
-  new THREE.BoxGeometry(3, 3, 3),
-  new THREE.MeshBasicMaterial({ map: logoTexture })
+    new THREE.BoxGeometry(3, 3, 3),
+    new THREE.MeshBasicMaterial({ map: logoTexture })
 );
 
 logo.rotation.x += 20;
@@ -152,11 +230,11 @@ const moonTexture = new THREE.TextureLoader().load("https://the-nova-system.gith
 const normalTexture = new THREE.TextureLoader().load("https://the-nova-system.github.io/extra-assets/normal.jpg");
 
 const moon = new THREE.Mesh(
-  new THREE.SphereGeometry(3, 32, 32),
-  new THREE.MeshStandardMaterial({
-    map: moonTexture,
-    normalMap: normalTexture,
-  })
+    new THREE.SphereGeometry(3, 32, 32),
+    new THREE.MeshStandardMaterial({
+      map: moonTexture,
+      normalMap: normalTexture,
+    })
 );
 
 scene.add(moon);
@@ -170,7 +248,8 @@ logo.position.x = 2;
 // Scroll Animation
 
 function moveCamera() {
-  const t = document.body.getBoundingClientRect().top;
+  let t = document.body.getBoundingClientRect().top;
+
   moon.rotation.x += 0.05;
   moon.rotation.y += 0.075;
   moon.rotation.z += 0.05;
@@ -189,20 +268,20 @@ moveCamera();
 // Page Load Function
 window.onload = function() {
   if (location.hash === '#contact') {
-    console.log(location.hash);
     contactForm('home-page', 'contact-page');
   } else if (location.hash === '#team') {
-    console.log(location.hash);
     teamPage('home-page', 'team-page');
+  } else if (location.hash === '#blog') {
+    blogPage('home-page', 'blog-page');
   }
 
   const a = document.getElementById("contact-form");
   const b = document.getElementById("home");
   const c = document.getElementById("home-named");
   const d = document.getElementById("our-team-button");
+  const e = document.getElementById("blog-button");
 
   a.onclick = function() {
-    console.log(location.hash);
     let initialPage = '';
     if (location.hash === '')  {
       initialPage = 'home-page';
@@ -217,7 +296,6 @@ window.onload = function() {
   }
 
   d.onclick = function() {
-    console.log(location.hash);
     let initialPage = '';
     if (location.hash === '')  {
       initialPage = 'home-page';
@@ -228,6 +306,20 @@ window.onload = function() {
       teamPage(initialPage, 'team-page');
     }
     location.hash = "#team";
+    return false;
+  }
+
+  e.onclick = function() {
+    let initialPage = '';
+    if (location.hash === '')  {
+      initialPage = 'home-page';
+    } else {
+      initialPage = location.hash.substring(1) + '-page';
+    }
+    if (location.hash !== '#team') {
+      blogPage(initialPage, 'blog-page');
+    }
+    location.hash = "#blog";
     return false;
   }
 
@@ -346,6 +438,58 @@ function teamPage(input, output) { //'home-page', 'team-page'
     }
 
     team += 1;
+    TWEEN.update();
+
+    renderer.render(scene, camera);
+  }
+}
+
+function blogPage(input, output) {
+  if (blog === -1) {
+    blog = 0;
+  } else {
+    requestAnimationFrame(blogPage);
+
+    if (blog === 0) {
+      doTransition(document.getElementById(input), document.getElementById(output), false);
+
+      let position = {x: camera.position.x, y: camera.position.y, z: camera.position.z};
+      let target = {x: -10, y: -30, z: 18};
+      const tween = new TWEEN.Tween(position).to(target, 2000);
+      tween.easing(TWEEN.Easing.Quadratic.InOut)
+      tween.onUpdate(function () {
+        camera.position.x = position.x;
+        camera.position.y = position.y;
+        camera.position.z = position.z;
+      });
+
+
+      let startRotation = new THREE.Euler().copy(camera.rotation);
+      camera.lookAt(new THREE.Vector3(target.x, target.y, target.z));
+      let endRotation = new THREE.Euler().copy(camera.rotation);
+      camera.rotation.copy(startRotation);
+      const rotationTween = new TWEEN.Tween(startRotation).to({
+        x: endRotation.x,
+        y: endRotation.y,
+        z: endRotation.z
+      }, 2000);
+      rotationTween.easing(TWEEN.Easing.Quadratic.InOut);
+      rotationTween.onUpdate(function () {
+        camera.rotation.x = startRotation.x;
+        camera.rotation.y = startRotation.y;
+        camera.rotation.z = startRotation.z;
+      });
+
+      tween.onComplete(function () {
+        fetchPosts()
+        blog = -1;
+      });
+
+      tween.start();
+      rotationTween.start();
+    }
+
+    blog += 1;
     TWEEN.update();
 
     renderer.render(scene, camera);
